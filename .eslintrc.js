03b8900c923e789f7665fdf05f7d80026a012db6
luckyman1004 <luckysuperdev@gmail.com>
2018-12-18 12:59:18 +0530

    Configured .eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}],
+    "no-console": 0,
+    "no-alert": 0,
+    "no-case-declarations": 0,
+  },
+};
